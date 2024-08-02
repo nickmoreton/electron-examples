d87c04e7763a1316a1274cd6ab6ca7d94e2c582f
nickmoreton <nick.moreton@torchbox.com>
2024-08-02 22:45:30 +0100

    Quick start

---
+++ 1_quick_start/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// Add more code here to run in the main process.
