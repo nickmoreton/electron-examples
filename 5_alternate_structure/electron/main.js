1ed6d3bfb333be29cfac5f330a15519edcf56554
nickmoreton <nick.moreton@torchbox.com>
2024-08-21 14:12:48 +0100

    An alternate folder structure for the Electron app

---
+++ 5_alternate_structure/electron/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
+
+/**
+ * Creates a browser window and loads the index.html of the app.
+ * @function createWindow
+ * @returns {void}
+ */
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+
+  mainWindow.loadFile('src/index.html')
+
+
+  mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
